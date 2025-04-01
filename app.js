13f3b1a96eda71b7f29798ff606c7e593a6f730e
aidanbeck <aidanmbeck@outlook.com>
2025-03-31 21:59:35 -0500

    completed exercise

---
+++ app.js
@@ -0,0 +1,10 @@
+let items = ["coffee", "tea", "soil", "mustard", "beet juice"];
+let prices = [52, 38, .99];
+
+for (i of items) { console.log(i); }
+for (i of prices) { console.log(i); }
+console.log(items[0]);
+console.log(items[2]);
+console.log(items[3]);
+console.log(prices[0]);
+console.log(prices[2]);
